a48384eeea2837d8f4a77e63b21ede7958a72351
nmrmasakazu <m.nomura.733@nitech.jp>
2021-04-01 09:40:59 +0900

    first commit

--- yahoo-search-anywhere Extension/SafariExtensionHandler.swift
+++ yahoo-search-anywhere Extension/SafariExtensionHandler.swift
@@ -13,9 +13,10 @@ class SafariExtensionHandler: SFSafariExtensionHandler {
         // This method will be called when a content script provided by your extension calls safari.extension.dispatchMessage("message").
         page.getPropertiesWithCompletionHandler { properties in
             NSLog("The extension received a message (\(messageName)) from a script injected into (\(String(describing: properties?.url))) with userInfo (\(userInfo ?? [:]))")
+            print("koko")
         }
     }
-
+    
     override func toolbarItemClicked(in window: SFSafariWindow) {
         // This method will be called when your toolbar item is clicked.
         NSLog("The extension's toolbar item was clicked")
@@ -29,5 +30,17 @@ class SafariExtensionHandler: SFSafariExtensionHandler {
     override func popoverViewController() -> SFSafariExtensionViewController {
         return SafariExtensionViewController.shared
     }
+    
+    override func contextMenuItemSelected(withCommand command: String, in page: SFSafariPage, userInfo: [String : Any]? = nil) {
+        NSLog("contextMenuItemSelected : Command : \(command), UserInfo : \(String(describing: userInfo))")
 
+        if command == "GoogleImageSearch" {
+            let googleSearchUrl = "https://www.google.com/searchbyimage?&image_url=\(userInfo!["imageSrc"])&safe=off";
+            SFSafariApplication.getActiveWindow { (activeWindow) in
+                activeWindow?.openTab(with: URL(string: googleSearchUrl)!, makeActiveIfPossible: true, completionHandler: {_ in
+                    NSLog("Opened google image search url : \(googleSearchUrl)")
+                })
+            }
+        }
+    }
 }
---
+++ yahoo-search-anywhere Extension/script.js
@@ -0,0 +1,9 @@
+document.addEventListener("DOMContentLoaded", function(event) {
+
+});
+
+document.addEventListener("contextmenu", function (event) {
+    console.log(event)
+});
+
+console.log("yahoo-search-anywhere loaded!");
